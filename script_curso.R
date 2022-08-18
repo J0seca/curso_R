931061e6329cf12314f8b59257aca2fea31f15af
J0seca <42120345+J0seca@users.noreply.github.com>
2022-08-18 18:40:23 -0400

    Update script_curso.R

--- script_curso.R
+++ script_curso.R
@@ -81,3 +81,34 @@ is.numeric(falso)
 is.character(falso)
 is.logical(falso)
 
+
+# Array: Un array es un vector extendido a más de dos dimensiones
+# Se usan para representar datos multidimensionales de un único tipo
+
+# se crean usando la función array(datos, dimensiones)
+
+# para aplicar funciones a los elementos del array se usa la funcion
+# apply(nombre, dimensión, función)
+
+# para acceder a los elementos del array se usa dimnames(x)
+
+# podemos comprobar si un elemento es un array con is.array()
+
+
+x<-array(1:12,c(3,1,4))
+is.array(x)
+x
+
+mean(x)
+min(x)
+max(x)
+
+
+# Matriz: extensión de un vector a dos dimensiones, por lo que dispone del atributo dimensión
+# 2 DIMENSIONES: filas (alto) y columnas (largo)
+# a diferencia de un array la matriz representa datos de un único tipo en dos dimensiones
+
+# Las matrices se crean con la funcion matrix(data, nrow, ncol, byrow=F)
+
+nueva_matriz<-matrix(1:6,nrow=3,ncol=2, byrow=T)
+nueva_matriz
